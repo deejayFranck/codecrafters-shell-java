89f003a522c4b85643ef1e495fde526816286d5a
Franck Djatcha <djatchafranck@gmail.com>
2025-11-23 16:38:26 +0100

    handle all commands as invalid

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         System.out.print("$ ");
+        // Captures the user's command in the "command" variable
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        // Prints the "<command>: command not found" message
+        System.out.println(command + ": command not found");
     }
 }
